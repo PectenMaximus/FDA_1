708f2989aeda1d7aa29fb01efdacf78b895915f3
Alec Reid <muckrossreid@gmail.com>
2024-01-06 15:30:58 +0000

    Indv Irises Plots

--- FDA1_Code.py
+++ FDA1_Code.py
@@ -1,14 +1,29 @@
-from ucimlrepo import fetch_ucirepo 
+#Imports
 
-# fetch dataset 
-iris = fetch_ucirepo(id=53) 
-  
-# data (as pandas dataframes) 
-X = iris.data.features 
-y = iris.data.targets 
-  
-# metadata 
-print(iris.metadata) 
-  
-# variable information 
-print(iris.variables) 
+import pandas as pd
+import numpy as np 
+import os
+import matplotlib.pyplot as plt
+
+# Loading the Iris Data Set
+IrisData = pd.read_csv('Iris.csv')
+IrisData.head()
+
+# Dropping unwanted columns
+IrisData= IrisData.drop(columns= ['Id'])
+IrisData.head()
+
+# Print info on data frame
+IrisData.info()
+
+# Identify null values
+IrisData.isnull().sum()
+
+# Plotting Histogram of sepal width for all irises
+x = np.array(IrisData['SepalWidthCm'])
+plt.title("Sepal Width for All Irises", loc = 'center')
+plt.xlabel("Sepal Width Cm")
+plt.ylabel("No. Irises")
+
+plt.hist(x, color= 'orange', edgecolor='purple')
+plt.show()
